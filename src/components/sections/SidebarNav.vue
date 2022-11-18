e943ca600999921d82aeac0effbfbce66e45d76c
Muhammad A.G. Bello <magbello157@gmail.com>
2022-11-18 12:48:07 +0100

    add dynamic toggling of active menu

--- src/components/sections/SidebarNav.vue
+++ src/components/sections/SidebarNav.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="sidebar-nav-wrapper">
     <ul class="sidebar-nav">
-      <li>Overview</li>
-      <li class="active">Service tokenomics</li>
-      <li>Node configuration</li>
-      <li>API keys</li>
-      <li>Node telemetry</li>
-      <li>Logs</li>
-      <li>Backups</li>
+      <li @click="activate(1)" :class="{ active: activeElement === 1 }">Overview</li>
+      <li @click="activate(2)" :class="{ active: activeElement === 2 }">Service tokenomics</li>
+      <li @click="activate(3)" :class="{ active: activeElement === 3 }">Node configuration</li>
+      <li @click="activate(4)" :class="{ active: activeElement === 4 }">API keys</li>
+      <li @click="activate(5)" :class="{ active: activeElement === 5 }">Node telemetry</li>
+      <li @click="activate(6)" :class="{ active: activeElement === 6 }">Logs</li>
+      <li @click="activate(7)" :class="{ active: activeElement === 7 }">Backups</li>
     </ul>
   </div>
 </template>
@@ -15,6 +15,16 @@
 <script>
 export default {
   name: 'SidebarNav',
+  data() {
+    return {
+      activeElement: 1,
+    };
+  },
+  methods: {
+    activate: function (element) {
+      this.activeElement = element;
+    },
+  },
 };
 </script>
 
