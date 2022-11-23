8ae6378bdd2b28f1dc5ac886cf09e3553a430d14
Muhammad A.G. Bello <magbello157@gmail.com>
2022-11-23 08:15:58 +0100

    improve rpc status alignment

--- src/components/sections/Header.vue
+++ src/components/sections/Header.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="header-wrapper">
-    <img class="logo" src="../../../public/images/logo.svg" alt="Houston application logo" />
-
-    <div class="node-info">
-      <info-pair
-        title="Node version"
-        description="v6-beta.3.0.1 (autoupdate <b>ON</b>)"
-      ></info-pair>
-      <info-pair title="Latest version" description="v6-beta.3.0.1"></info-pair>
-      <info-pair title="Last start time" description="2022-11-08 12:31 (1667939511)"></info-pair>
-    </div>
-
-    <div class="assets-info">
-      <info-pair title="Number of hosted assets" description="80219"></info-pair>
-      <info-pair title="Stake" description="50 500 TRAC (Delegated 0 TRAC)"></info-pair>
-      <info-pair title="Operational wallet" description="0x1234563234123411334"></info-pair>
+    <div class="logo-node-and-assets-info">
+      <img class="logo" src="../../../public/images/logo.svg" alt="Houston application logo" />
+      <div class="node-info">
+        <info-pair
+          title="Node version"
+          description="v6-beta.3.0.1 (autoupdate <b>ON</b>)"
+        ></info-pair>
+        <info-pair title="Latest version" description="v6-beta.3.0.1"></info-pair>
+        <info-pair title="Last start time" description="2022-11-08 12:31 (1667939511)"></info-pair>
+      </div>
+      <div class="assets-info">
+        <info-pair title="Number of hosted assets" description="80219"></info-pair>
+        <info-pair title="Stake" description="50 500 TRAC (Delegated 0 TRAC)"></info-pair>
+        <info-pair title="Operational wallet" description="0x1234563234123411334"></info-pair>
+      </div>
     </div>
 
     <div class="blockchain-rpc-status">
@@ -49,6 +49,7 @@ export default {
 
 .header-wrapper {
   display: flex;
+  justify-content: space-between;
   align-items: center;
   height: 128px;
   padding: 20px 30px;
@@ -58,25 +59,29 @@ export default {
     height: 42px;
   }
 
-  .node-info {
+  .logo-node-and-assets-info {
     display: flex;
-    flex-direction: column;
-    height: 56px;
-    margin-left: 114px;
-    row-gap: 4px;
-  }
 
-  .assets-info {
-    display: flex;
-    flex-direction: column;
-    height: 56px;
-    margin-left: 18px;
-    row-gap: 4px;
+    .node-info {
+      display: flex;
+      flex-direction: column;
+      height: 56px;
+      margin-left: 114px;
+      row-gap: 4px;
+    }
+
+    .assets-info {
+      display: flex;
+      flex-direction: column;
+      height: 56px;
+      margin-left: 18px;
+      row-gap: 4px;
+    }
   }
 
   .blockchain-rpc-status {
     height: 98px;
-    margin-left: 260px;
+    margin-right: 40px;
 
     p {
       font-style: normal;
