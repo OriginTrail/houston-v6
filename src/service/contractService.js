2564eaf0e683fdcd508cb2eb7a886f7fd89cd1f2
Moda20 <kadhem03@gmail.com>
2023-01-17 09:19:48 +0100

    remove big.js dependency and replace its usage with internal ethers utils

--- src/service/contractService.js
+++ src/service/contractService.js
@@ -13,14 +13,12 @@ import ERC20Token from '../abis/ERC20Token.json';
 import { ethers } from 'ethers';
 import store from '../store';
 
-import { ADMIN_KEY_PURPOSE, BIG_PE_NUMBER } from '@/utils/constants';
+import { ADMIN_KEY_PURPOSE } from '@/utils/constants';
 import { getAmountWithDecimals } from '@/utils/cryptoUtils';
-import Big from 'big.js';
 
 class ContractService {
   constructor(ethersSigner) {
     this.ethersSigner = ethersSigner;
-    Big.PE = BIG_PE_NUMBER;
   }
 
   async getContractAddress(contractName) {
@@ -181,11 +179,10 @@ class ContractService {
 
     const totalSupply = await shareContract.totalSupply();
     const totalStakes = await this.getTotalStake(identityId);
-    const sharesToBurn = Big(getAmountWithDecimals(stakeToWithdraw))
+    const sharesToBurn = ethers.utils
+      .parseUnits(stakeToWithdraw, 18)
       .mul(totalSupply)
-      .div(totalStakes)
-      .round()
-      .toString();
+      .div(totalStakes);
 
     await (
       await shareContract.increaseAllowance(stakingContractAddress, sharesToBurn, {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,1 @@
 export const ADMIN_KEY_PURPOSE = 1;
-export const BIG_PE_NUMBER = 10000; // The maximum number of digits before we start using exponent notation
